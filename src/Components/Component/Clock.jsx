78ddba0e38004dcf8c37f63ade7b534580563a9a
CanOguzz <mustafacanoguz@gmail.com>
2024-03-12 15:15:56 +0300

    Add break and session length functionality to Clock component handleFunctions added

--- src/Components/Component/Clock.jsx
+++ src/Components/Component/Clock.jsx
@@ -1,53 +1,130 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
 import "./Clock.css";
 
-
-
 export const Clock = () => {
+  const handleBreakIncrease = () => {
+    //used 60 because we represent time in seconds and it cant be bigger than 1 hour
+    if (breakLength < 60) {
+      setBreakLength(breakLength + 1);
+    }
+  };
+  //it cant go lower than 1 minute
+  const handleBreakDecrease = () => {
+    if (breakLength > 1) {
+      setBreakLength(breakLength - 1);
+    }
+  };
 
-    const{handleBreakIncrease, handleBreakDecrease, handleSessionIncrease, 
-        handleSessionDecrease, handlePlay, handleReset, play, breakLength, 
-        sessionLength, title, timeLeft, timeFormatter=()=>"25:00"} = {}
-        
+  const handleSessionIncrease = () => {
+    if (sessionLength < 60) {
+      setSessionLength(sessionLength + 1);
+    }
+  };
+
+  const handleSessionDecrease = () => {
+    if (sessionLength > 1) {
+      setSessionLength(sessionLength - 1);
+    }
+  };
+
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [play, setPlay] = useState(false);
+  const [timingType, setTimingType] = useState("Session");
+  const [timeLeft, setTimeLeft] = useState(sessionLength * 60);
+
+  const timeFormatter = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft - minutes * 60;
+    const formatedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    const formatedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${formatedMinutes}:${formatedSeconds}`;
+  };
+
+  const timeout = setTimeout(() => {
+    if (timeLeft && play) {
+      setTimeLeft(timeLeft - 1);
+    }
+  }, 1000);
+  const handlePlay = () => {
+    clearTimeout(timeout);
+    if (play) {
+      setPlay(false);
+    } else {
+      setPlay(true);
+    }
+  };
+
+  const { handleReset } = {};
+
+  const title = timingType === "Session" ? "Session" : "Break";
   return (
     <div>
-    <div className="wrapper">
-      <h2>25 + 5 Clock</h2>
-      <div className="break-session-length">
-        <div>
-          <h3 id="break-label">Break Length</h3>
+      <div className="wrapper">
+        <h2>25 + 5 Clock</h2>
+        <div className="break-session-length">
           <div>
-            <button disabled={play} onClick={handleBreakIncrease} id="break-increment">Increase</button>
+            <h3 id="break-label">Break Length</h3>
+            <div>
+              <button
+                disabled={play}
+                onClick={handleBreakIncrease}
+                id="break-increment"
+              >
+                Increase
+              </button>
               <strong id="break-length">{breakLength}</strong>
-            <button disabled={play} onClick={handleBreakDecrease} id="break-decrement">Decrease</button>
+              <button
+                disabled={play}
+                onClick={handleBreakDecrease}
+                id="break-decrement"
+              >
+                Decrease
+              </button>
+            </div>
           </div>
-         </div>
-         <div>
-           <h3 id="session-label">Session Length</h3>
-           <div>
-            <button disabled={play} onClick={handleSessionIncrease} id="session-increment">Increase</button>
+          <div>
+            <h3 id="session-label">Session Length</h3>
+            <div>
+              <button
+                disabled={play}
+                onClick={handleSessionIncrease}
+                id="session-increment"
+              >
+                Increase
+              </button>
               <strong id="session-length">{sessionLength}</strong>
-            <button disabled={play} onClick={handleSessionDecrease} id="session-decrement">Decrease</button>
+              <button
+                disabled={play}
+                onClick={handleSessionDecrease}
+                id="session-decrement"
+              >
+                Decrease
+              </button>
+            </div>
           </div>
-         </div>
-      </div>
-      <div className="timer-wrapper">
-        <div className="timer">
-           <h2 id="timer-label">{title}</h2>
-           <h3 id="time-left">{timeFormatter()}</h3>
         </div>
-        <button onClick={handlePlay} id="start_stop">Start/Stop</button>
-        <button onClick={handleReset} id="reset">Reset</button>
+        <div className="timer-wrapper">
+          <div className="timer">
+            <h2 id="timer-label">{title}</h2>
+            <h3 id="time-left">{timeFormatter()}</h3>
+          </div>
+          <button onClick={handlePlay} id="start_stop">
+            Start/Stop
+          </button>
+          <button onClick={handleReset} id="reset">
+            Reset
+          </button>
+        </div>
       </div>
+      <audio
+        id="beep"
+        preload="auto"
+        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+      />
     </div>
-    <audio
-      id="beep" 
-      preload="auto"
-      src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
-    />
-    </div>);
-  
-}
+  );
+};
 
 export default Clock;
